2f6071f01fa18dcfe54d225c0689889fb408987a
sumpwa <143224090+sumpwa@users.noreply.github.com>
2023-10-25 09:31:39 -0500

    Reorg main program

--- main.cpp
+++ main.cpp
@@ -6,48 +6,35 @@
 
 using std::cout, std::cin, std::string, std::fixed, std::setprecision;
 
+double picalc(int pi_in){
+    double pie = 0.0;
+    double pi_a = 1.0;
+    double pi_b = 1.0 / sqrt(2);
+    double pi_t = 0.25;
+    double pi_p = 1.0;
+    double pi_anext = 0.0;
+    double pi_bnext = 0.0;
+    double pi_tnext = 0.0;
+    double pi_pnext = 0.0;
+    double pi_pie = 0.0;
+    while(pi_in<4) {
+        
+        pi_anext = (pi_a + pi_b) / 2.0;
+        pi_bnext = sqrt(pi_a * pi_b);
+        pi_tnext = pi_t - pi_p * (pi_a - pi_anext) * (pi_a - pi_anext);
+        pi_pnext = 2.0 * pi_p;
+        pie = ((pi_a + pi_b) * (pi_a + pi_b)) / (4.0 * pi_t);
+        pi_a = pi_anext;
+        pi_b = pi_bnext;
+        pi_t = pi_tnext;
+        pi_p = pi_pnext;
 
-
-double picalc(int pi_in) {
-double pie = 0.0;
-double pi_a = 1.0;
-double pi_b = 1.0 / sqrt(2);
-double pi_t = 0.25;
-double pi_p = 1.0;
-double pi_anext = 0.0;
-double pi_bnext = 0.0;
-double pi_tnext = 0.0;
-double pi_pnext = 0.0;
-double pi_pie = 0.0;
-while(pi_in<4){
-    
-    pi_anext = (pi_a + pi_b)/2.0;
-    pi_bnext = sqrt(pi_a * pi_b);
-    pi_tnext = pi_t - pi_p * (pi_a - pi_anext)*(pi_a - pi_anext);
-    pi_pnext = 2.0 * pi_p;
-    pie = ((pi_a + pi_b)*(pi_a + pi_b)) / (4.0 * pi_t);
-    pi_a = pi_anext;
-    pi_b = pi_bnext;
-    pi_t = pi_tnext;
-    pi_p = pi_pnext;
-
-    pi_in++;
-    }
+        pi_in++;
+        }
     return pie;
 }
 
-string leftpad(string str, int len, char ch = ' ') {
-    if (ch == 0) ch = ' '; // Using space as default padding character
-    len -= str.length();
-    
-    while (len-- > 0) {
-        str = ch + str;
-    }
-    
-    return str;
-}
-
-float Q_rsqrt(float number) {
+float Q_rsqrt(float number){
 
     long i;
     float x2, y;
@@ -87,8 +74,67 @@ float Q_rsqrt(float number) {
     
 }
 
+uint64_t fibonacci(int input){
+    int count = 1;
+    uint64_t first = 0;
+    uint64_t last = 0;
+    uint64_t fibo = 1;
 
-int main() {
+    while (count < input){
+
+        cout << fibo << '\n';
+        first = last;
+        last = fibo;
+        fibo = first + last;
+        count++;
+        }
+    
+    return fibo;
+}
+
+double euler(int input){
+    int eulercount = 0;
+    double eulerresult = 0.0;
+    while( eulercount <= input ){
+        eulerresult = eulerresult + 1.0 / tgamma(eulercount);
+        eulercount++;
+    }
+    return eulerresult;
+}
+
+string fizzbuzz(int fizzin, int buzzin){
+
+    int fizzcount = 0;
+    int buzzcount = 0;
+    int fbcount = 0;
+    int nFB = 1;
+
+    for (nFB; nFB <= 100; nFB++){
+
+        if((nFB % fizzin==0) && (nFB %buzzin==0)){
+            cout << "Fizzbuzz!" << '\n';
+            fbcount++;
+        }
+        else if(nFB % buzzin == 0){
+            buzzcount++;
+            cout << "Buzz!" << '\n';
+        }
+        else if(nFB % fizzin == 0){
+            fizzcount++;
+            cout << "Fizz!" << '\n';
+        }
+        else{
+            cout << nFB << '\n';
+        }
+    }
+
+    cout << "Fizz count: " << fizzcount;
+    cout << '\n' << "Buzz count: " << buzzcount;
+    cout << '\n' << "Fizzbuzz count: " << fbcount << '\n';
+    return "End of Fizzbuzz";
+}
+
+int main(){
     cout << "Hello World!" << '\n' << '\n';
 
     float result;
@@ -97,7 +143,6 @@ int main() {
     cout << "Enter a number to calculate the inverse square root: " << '\n' << '\n';
     cin >> number;
     
-
     result = Q_rsqrt(number);
 
     cout << fixed;
@@ -107,94 +152,32 @@ int main() {
     cout << fixed;
     cout << setprecision(12);
     cout << "Compared to standard C inverse square root function: " << SquareRoot << '\n' << '\n';
-
     
     int fibiter = 0;
     cout << '\n' << "Enter how many iterations of the fibonacci sequence to calculate: " << '\n';
     cin >> fibiter;
-    int count = 0;
-    uint64_t first = 0;
-    uint64_t last = 0;
-    uint64_t fibo = 0;
-
-while (fibiter >= count) 
-{
-    if (fibo == 0) {
-        cout << '\n' << fibo;
-        fibo = 1;
-    }
-    else {
-    cout << '\n' << fibo;
-    first = last;
-    last = fibo;
-    fibo = first + last;
-    }
-    count++;
-}
-
+    cout << fibonacci(fibiter) << '\n';
     
     cout << '\n' << "Now let's calculate Euler's number";
-    int eulercount = 0;
+    int eulersteps = 0;
     cout << '\n' << "Enter how many steps to perform:";
-    cin >> eulercount;
-
-    double euler = 0.0;
-    int eulersteps = 1;
-    while(eulersteps<=eulercount){
-    euler = euler + 1.0 / tgamma(eulersteps);
-    eulersteps = eulersteps+1;
-    
-    }
-
+    cin >> eulersteps;
     cout << fixed;
     cout << setprecision(12);
-    cout << '\n' << "Euler's Number: " << euler;
-
-
-    
-
-
-    cout << '\n' << "Leftpad Demonstration" << '\n';
-    string LeftInput = "hello";
-    int desiredLength = 10;
-    char paddingChar = '*';
-    
-    string output = leftpad(LeftInput, desiredLength, paddingChar);
-    cout << output << '\n';
-
-    
+    cout << "Euler's Number: " << euler(eulersteps) << '\n';
 
     int FIZZARG = 0;
     int BUZZARG = 0;
-    int fizzcount = 0;
-    int buzzcount = 0;
-    int fbcount = 0;
 
-    cout << "Enter values for Fizz and Buzz: " << '\n';
+    cout << '\n' << "Enter values for Fizz and Buzz: " << '\n';
     cin >> FIZZARG >> BUZZARG;
-
-    for (int nFB = 1; nFB <= 100; nFB++){
-        if((nFB%FIZZARG==0)&&(nFB%BUZZARG==0)){
-            cout << nFB << " Fizzbuzz!" << '\n';
-            fbcount++;
-        }
-        else if(nFB%BUZZARG==0){
-            buzzcount++;
-            cout << nFB << " Buzz!" << '\n';
-        }
-        else if(nFB%FIZZARG==0){
-            fizzcount++;
-            cout << nFB << " Fizz!" << '\n';
-        }
-
-    }
-    cout << "Fizz count: " << fizzcount;
-    cout << '\n' << "Buzz count: " << buzzcount;
-    cout << '\n' << "Fizzbuzz count: " << fbcount;
-  
+    cout << '\n' << "Running Fizzbuzz..." << '\n';
+    cout << fizzbuzz(FIZZARG, BUZZARG);
 
     int pi_input = 0;
-    cout << '\n' << "Pi is: " << picalc(pi_input) << '\n';
+    cout << '\n';
+    cout << '\n' << "Caclulating pi with the Gauss-Legendere Algorithm with 4 steps";
+    cout << '\n' << "Pi is: " << picalc(pi_input);
     cout << '\n' << "Compared to the built in pi function: " << M_PI << '\n';
 
     cout << '\n' << "End of program";
